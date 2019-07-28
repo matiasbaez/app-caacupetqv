4432b42ce976950aef9b5df419e4ef2419ebc589
Matias Báez <matiasbaez2512@gmail.com>
2019-07-28 13:59:53 -0400

    Add interfaces

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -1,6 +1,40 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    image: string;
+    estado: number;
+}
+
+export interface Role {
+    idRole: string;
+    nombre: string;
+    flag: string;
+    estado: number;
+}
+
 export interface Plant {
+    idPlanta: string;
     nombre: string;
     descripcion: string;
     imagen: string;
-    estado: boolean;
+    estado: number;
+}
+
+export interface Zones {
+    idZona: string;
+    nombre: string;
+    estado: number;
+}
+
+export interface Publications {
+    idPlublicacion: string;
+    idUsuario: string;
+    idPlanta: string;
+    idZona: string;
+    descripcion: string;
+    latLng: string;
+    estado: number;
 }
