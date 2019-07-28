d465eaef0fe44052471f0428ed1180100acfde19
Matias Báez <matiasbaez2512@gmail.com>
2019-07-28 14:00:47 -0400

    Add Ui and User service

---
+++ src/app/services/ui.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UIService {
+
+  constructor(
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController
+  ) {}
+
+  async showAlert(message: string) {
+    const alert = await this.alertCtrl.create({
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1500
+      // position: 'top'
+    });
+    await toast.present();
+  }
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,120 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { environment } from '../../environments/environment';
+import { User } from '../interfaces/interfaces';
+
+const API = environment.api;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  token: string = null;
+  emmitter = new EventEmitter();
+  private user: User = null;
+
+  constructor(
+    private http: HttpClient,
+    private storage: Storage,
+    private navCtrl: NavController,
+    private googlePlus: GooglePlus
+  ) {}
+
+  login(data) {
+    return new Promise(resolve => {
+      this.http.post(`${API}/login`, data).subscribe(
+        async (response: any) => {
+          if (response.token) {
+            await this.saveToken(response.token);
+            this.emmitter.emit(this.user);
+            resolve(true);
+          } else {
+            this.token = null;
+            await this.storage.remove('token');
+            resolve(false);
+          }
+        },
+        (error) => {
+          resolve(false);
+        }
+      );
+    });
+  }
+
+  googleLogin() {
+    this.googlePlus.login({})
+    .then(response => {
+      console.log('response: ', response);
+    })
+    .catch(err => {
+      console.log('Error: ', err);
+    });
+  }
+
+  register(data) {
+    return new Promise(resolve => {
+      this.http.post(`${API}/register`, data).subscribe(
+        async (response: any) => {
+          if (response.token) {
+            await this.saveToken(response.token);
+            resolve(true);
+          } else {
+            this.token = null;
+            await this.storage.remove('token');
+            resolve(false);
+          }
+        },
+        (error) => {
+          resolve(false);
+        }
+      );
+    });
+  }
+
+  getUser() {
+    if (!this.user.id) {
+      this.validateToken();
+    }
+    return { ...this.user };
+  }
+
+  async saveToken(token: string) {
+    this.token = token;
+    await this.storage.set('token', token);
+    await this.validateToken();
+  }
+
+  async loadToken() {
+    this.token = await this.storage.get('token') || null;
+  }
+
+  async validateToken(): Promise<boolean> {
+    console.log('VALIDATE TOKEN');
+    await this.loadToken();
+    if (!this.token) {
+      this.navCtrl.navigateRoot('/tabs/home');
+      return Promise.resolve(false);
+    }
+
+    return new Promise<boolean>(resolve => {
+      const headers = new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer ' + this.token);
+      this.http.get(`${API}/user`, { headers }).subscribe(
+        (response: any) => {
+          console.log('response: ', response);
+          if (response.user) {
+            this.user = response.user;
+            resolve(true);
+          } else {
+            resolve(false);
+          }
+        }
+      );
+    });
+  }
+}
