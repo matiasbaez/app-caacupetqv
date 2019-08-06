b9c7784e4cf00b18116d9edafa5a988ce010a11d
Matias Báez <matiasbaez2512@gmail.com>
2019-08-06 16:13:33 -0400

    Upload file method

--- src/app/services/plants.service.ts
+++ src/app/services/plants.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { UserService } from './user.service';
+import { File, FileEntry } from '@ionic-native/file/ngx';
+import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Plant } from '../interfaces/interfaces';
+import { UserService } from './user.service';
+import { UIService } from './ui.service';
 
 const API = environment.api;
 
@@ -13,19 +15,42 @@ export class PlantsService {
 
   constructor(
     private http: HttpClient,
-    private userService: UserService
+    private userService: UserService,
+    private file: File,
+    private uiService: UIService
   ) { }
 
   getPlants() {
     return this.http.get<Plant>(`${API}/plantas`);
   }
 
-  addPlant(data: Plant) {
+  async readImage(imgPath, callback) {
+    await this.file.resolveLocalFilesystemUrl(imgPath)
+    .then(entry => {
+      (entry as FileEntry).file(async (file) => await this.readFile(file, callback));
+    })
+    .catch(err => {
+      this.uiService.showToast('Ha ocurrido un error al leer el archivo.');
+    });
+  }
+
+  private async readFile(file: any, callback) {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const formData = new FormData();
+      const imgBlob = new Blob([reader.result], { type: file.type });
+      formData.append('imagen', imgBlob, file.name);
+      callback(formData);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  addPlant(formData: FormData) {
     return new Promise(resolve => {
-      const headers = new HttpHeaders()
-        .append('Authorization', 'Bearer ' + this.userService.token);
-      this.http.post(`${API}/plantas`, data, {headers}).subscribe(
+      const headers = new HttpHeaders().append('Authorization', 'Bearer ' + this.userService.token);
+      this.http.post(`${API}/plantas`, formData, { headers }).subscribe(
         async (response: any) => {
+          console.log('response: ', response);
           if (response === 200) {
             resolve(true);
           } else {
