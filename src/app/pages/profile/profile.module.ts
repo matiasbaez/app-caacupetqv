aabe646fe7b1a0f323269ad2f106663bc9d51832
Matias Báez <matiasbaez2512@gmail.com>
2019-08-06 16:14:24 -0400

    Social Profile data

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { ProfilePage } from './profile.page';
 import { ComponentsModule } from './../../components/components.module';
+import { PipesModule } from '../../pipes/pipes.module';
 
 const routes: Routes = [
   {
@@ -20,6 +21,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    PipesModule,
     RouterModule.forChild(routes),
     ComponentsModule,
     ReactiveFormsModule
